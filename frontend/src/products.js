0089fea042fe1b66baa975dce2eb42ef6199b9c8
ShawonNSTU <shaonkantidhar07@gmail.com>
2023-04-14 00:13:25 +0600

    Product details screen

--- frontend/src/products.js
+++ frontend/src/products.js
@@ -4,18 +4,7 @@ const products = [
     name: 'Black Grapes',
     image: '/images/black-grapes.jpg',
     description:
-      "Black grapes, which are known for their velvety colour and sweet flavour, come packed with nutrients and antioxidants. With their cultivation dating back to 6,000 to 8,000 years ago, they are said to be the oldest cultivated fruit in the Near East and Europe, and are botanically known as Vitis vinifera. There are two widely known species of black grapes: the old world species is native to the area across the southeast coast of the Black Sea (near the south of the Caspian Sea) to Afghanistan and is still found in these regions with over 10,000 varieties; and the new world species that originated from South America and North Eastern America. Grapes are actually 'berries' that have a semi-solid, translucent flesh inside them; they may or may not have seeds. The most popular use of grapes, all over the world, is in making wines. However, the juicy fruit has also added a number of uses to its crowning glory. Apart from being relished as a fruit or in salads, black grapes can also be cooked in a variety of ways to make sauces, jams, compotes, desserts and more. In addition, it has some incredible benefits for the skin, hair and overall health." +
-      'Nutritional facts/Ingredients :' +
-      'Benefits:' +
-      '-Protects the Heart' +
-      '-Improves Vision' +
-      '-Fights Risk of Cancer' +
-      '-Boosts Brain Function' +
-      '-Prevents the Risk of Diabetes' +
-      '-Promotes Healthy Hair' +
-      '-Immunity Booster' +
-      '-Healthy Skin' +
-      '- Prevents Bone Loss',
+      'Black grapes, which are known for their velvety colour and sweet flavour, come packed with nutrients and antioxidants. The most popular use of grapes, all over the world, is in making wines. However, the juicy fruit has also added a number of uses to its crowning glory. Apart from being relished as a fruit or in salads, black grapes can also be cooked in a variety of ways to make sauces, jams, compotes, desserts and more. In addition, it has some incredible benefits for the skin, hair and overall health.',
     brand: '',
     category: 'Fruits',
     price: 120,
@@ -30,14 +19,13 @@ const products = [
     image: '/images/malta.jpg',
     description:
       'Malta, a variety of orange, is a popular citrus fruit cultivated in hot climate. The fruit is rich in mineral salts and other ingredients of nutritive value such as iron, lime, phosphorus, and vital minerals.' +
-      'Malta is loaded with vitamin C and vitamin B complex. It is a round-shaped, medium-sized fruit, weighting an average of 145.8 gm.' +
-      'It has a rough-surfaced orange ringed with a segment of around 9.67 centimeters and a sweet-tasting juice content of around 33.7 per cent.' +
+      'Malta is loaded with vitamin C and vitamin B complex. It is a round-shaped, medium-sized fruit.' +
       'The fruit is often used to make processed juice, jam, jelly and marmalade.',
     brand: '',
     category: 'Fruits',
     price: 150,
     minQuantity: 500,
-    countInStock: 12,
+    countInStock: 0,
     rating: 4,
     numReviews: 17,
   },
