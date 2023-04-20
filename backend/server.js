c69c6501ae18066418b12ab0689276a89541e40f
Shaon Kanti Dhar <shaon.kanti@Shaons-MacBook-Pro.local>
2023-04-20 02:17:26 -0700

    build fix on mac

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 dotenv.config()
+connectDB()
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 6000
 
 app.listen(
   PORT,
