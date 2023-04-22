a347f2726dbea9bf13bfbe29d0dd93d5686dcd78
ShawonNSTU <shuvroshawon9@gmail.com>
2023-04-22 01:21:47 -0700

    Added script to import data into mongo db

--- backend/data/products.js
+++ backend/data/products.js
@@ -4,7 +4,7 @@ const products = [
     image: '/images/black-grapes.jpg',
     description:
       'Black grapes, which are known for their velvety colour and sweet flavour, come packed with nutrients and antioxidants. The most popular use of grapes, all over the world, is in making wines. However, the juicy fruit has also added a number of uses to its crowning glory. Apart from being relished as a fruit or in salads, black grapes can also be cooked in a variety of ways to make sauces, jams, compotes, desserts and more. In addition, it has some incredible benefits for the skin, hair and overall health.',
-    brand: '',
+    brand: 'Local',
     category: 'Fruits',
     price: 120,
     minQuantity: 250,
@@ -19,7 +19,7 @@ const products = [
       'Malta, a variety of orange, is a popular citrus fruit cultivated in hot climate. The fruit is rich in mineral salts and other ingredients of nutritive value such as iron, lime, phosphorus, and vital minerals.' +
       'Malta is loaded with vitamin C and vitamin B complex. It is a round-shaped, medium-sized fruit.' +
       'The fruit is often used to make processed juice, jam, jelly and marmalade.',
-    brand: '',
+    brand: 'Imported',
     category: 'Fruits',
     price: 150,
     minQuantity: 500,
