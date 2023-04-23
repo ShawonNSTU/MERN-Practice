4aba89f7d11d541570d9c27354e50450180a7605
ShawonNSTU <shuvroshawon9@gmail.com>
2023-04-23 01:57:15 -0700

    custom error handler with middleware

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 connectDB()
@@ -15,3 +16,5 @@ app.listen(
 )
 
 app.use('/api/products', productRoutes)
+app.use(notFound)
+app.use(errorHandler)
